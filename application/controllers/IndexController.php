226bcf93edd475837b7e1be3b76bd40ad65904b6
LegendPJ <pierrejulien.martinez@gmail.com>
2012-04-09 22:34:47 +0200

    lib a virer et vue/action/controller affichage

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -12,6 +12,12 @@ class IndexController extends Zend_Controller_Action
     {
         $this->view->title = "Bienvenue chez XYLAGROUP";
         $this->view->subMenu = "Qui sommes-nous ?";
+        $this->view->module = $this->getRequest()->getModuleName(); 
+        // recupere le module
+        $this->view->controller = $this->getRequest()->getControllerName(); 
+        // recupere le controller
+        $this->view->action = $this->getRequest()->getActionName(); 
+        // recupere l'action
     }
 
 
--- public/js/menuActive.js
+++ public/js/menuActive.js
@@ -1,6 +1,11 @@
 jQuery(function($) {
-	$('.navMenu a').click(function(){
-		$('.navMenu a.current').removeClass('current');
+	console.log("debut fonction");
+	$('ul.navMenu a').click(function(){
+		console.log("juste avant la detection");
+		$('ul.navMenu a.current').removeClass('current');
+		console.log("on remove la classe");
 		$(this).addClass('current');
+		console.log("on add la classé");
 	}); 
+	console.log("fin de la fonction");
 });
